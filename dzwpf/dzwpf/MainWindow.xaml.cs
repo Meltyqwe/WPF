8143c67346cc86dd7c8bb7351fdf324d892a2219
Meltyqwe <46374419+Meltyqwe@users.noreply.github.com>
2024-09-21 12:57:20 +0500

    Add files via upload

---
+++ dzwpf/dzwpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dzwpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string Nazvanie = NazvanieTextBox.Text;
+            if (decimal.TryParse(RashodTextBox.Text, out decimal rashod))
+            {   
+                RashodTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение для расходов.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+    public class Project
+    {
+        public string Nazvanie { get; set; }
+        public decimal Rashod { get; set; }
+        public decimal Profit { get; set; }
+    }
+}
